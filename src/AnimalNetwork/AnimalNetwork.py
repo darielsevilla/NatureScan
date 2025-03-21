755e2a900ad3180dac8217a99375074359952031
Dariel Sevilla <darielsevilla@gmail.com>
2025-03-20 19:39:45 -0600

    dataset change, improved functionality

--- src/AnimalNetwork/AnimalNetwork.py
+++ src/AnimalNetwork/AnimalNetwork.py
@@ -13,34 +13,14 @@ class AnimalNetwork:
     def __init__(self):
         
         #enlaces de dataset para entrenar y probar
-        dir = '/kaggle/input/animals-detection-images-dataset/'
-        self.train_dir = os.path.join(dir, 'train')
-        self.test_dir = os.path.join(dir, 'test')
+        self.train_dir = 'C:/Users/darie/Downloads/raw-img/'
+        #self.train_dir = os.path.join(dir, 'train')
+        #self.test_dir = os.path.join(dir, 'test')
 
-        #enlaces para datasets que no me mata la memoria de la compu
-        ds = deeplake.load("hub://activeloop/animal10n-test")
         
-        #dataset de entrenamiento
-        self.dataset = ds.tensorflow().map(lambda sample: (
-                tf.image.resize(tf.cast(tf.expand_dims(sample['images'], axis=0) , tf.uint8), (64, 64)) / 255.0,  
-                tf.one_hot(tf.cast(sample['labels'], tf.int32), depth=10)
-        ))     
-
-        print("Dataset: ")
-        first_element = next(iter(self.dataset.take(1)))
-        image, label = first_element
-        print("Image:")
-        print(image)
-        print("Label:")
-        print(label)
-        #dataset de prueba
-        ts = deeplake.load("hub://activeloop/animal10n-test")
-        self.testset = ts.tensorflow().map(lambda sample: (
-                tf.image.resize(tf.cast(tf.expand_dims(sample['images'], axis=0) , tf.uint8), (64, 64)) / 255.0,  
-                tf.one_hot(tf.cast(sample['labels'], tf.int32), depth=10)    
-        ))
-        print("Test Dataset: ")
-        print(self.testset)
+        
+       
+  
 
     def loadNetwork(self): #le deseo la muerte a esta libreria basura que nunca agarra donde quiero
         base_dir = Path(os.getcwd()) 
@@ -67,70 +47,108 @@ class AnimalNetwork:
             self.createNetwork()
             return False
 
+  
+    
     def createNetwork(self):
+        #enlaces para datasets que no me mata la memoria de la compu
+        #dataset de entrenamiento
+        train_datagen = tf.keras.preprocessing.image.ImageDataGenerator(
+            rescale=1./255,
+        )
+
+        ##test_datagen = tf.keras.preprocessing.image.ImageDataGenerator(rescale=1./255)
+
+        # Loading the data
+        self.dataset = train_datagen.flow_from_directory(
+            self.train_dir,
+            target_size=(64, 64), 
+            class_mode='categorical'  
+        )
+  
+
+        def augment(image, label):
+            image = tf.image.resize(image, [128, 128])  # Keep high res initially
+            image = tf.image.random_flip_left_right(image)
+            image = tf.image.random_brightness(image, max_delta=0.05)
+            
+            image = tf.image.random_contrast(image, lower=0.7, upper=1.3)
+            image = tf.image.adjust_contrast(image, contrast_factor=2.0)
+            image = tf.image.resize(image, [64, 64])  
+
+                
+            return image, label
+
+        #self.dataset = self.dataset.map(augment)
         
         #Dense -> que todas las neuronas del layer anterior estan conectadas a todas las del layer actual
         #Flatten -> cque convierte data de 2D (como una foto de mxn dimensiones) en un arreglo 1D, osea q toda el valor de cada pixel y lo pone en un arreglo que puede usar de input para la red neuronal
         self.model = tf.keras.Sequential([
           
             tf.keras.layers.Conv2D(32, (3,3),padding = 'same',  activation=tf.keras.layers.LeakyReLU(alpha=0.1), input_shape=(64,64,3)),
+            tf.keras.layers.BatchNormalization(),
+            tf.keras.layers.Conv2D(32, (3,3),padding = 'same',  activation=tf.keras.layers.LeakyReLU(alpha=0.1)),
+            tf.keras.layers.BatchNormalization(),
             tf.keras.layers.MaxPooling2D(2,2),
+
             tf.keras.layers.Conv2D(64, (3,3),  activation=tf.keras.layers.LeakyReLU(alpha=0.1), padding = 'same'),
+            tf.keras.layers.BatchNormalization(),
+             tf.keras.layers.Conv2D(64, (3,3),  activation=tf.keras.layers.LeakyReLU(alpha=0.1), padding = 'same'),
+            tf.keras.layers.BatchNormalization(),
             tf.keras.layers.MaxPooling2D(2,2),
+
             tf.keras.layers.Conv2D(128, (3,3),  activation=tf.keras.layers.LeakyReLU(alpha=0.1),padding = 'same'),
+            tf.keras.layers.BatchNormalization(),
+            tf.keras.layers.Conv2D(128, (3,3),  activation=tf.keras.layers.LeakyReLU(alpha=0.1),padding = 'same'),
+            tf.keras.layers.BatchNormalization(),
             tf.keras.layers.MaxPooling2D(2,2),
-            tf.keras.layers.Conv2D(512, (3,3), activation=tf.nn.swish),
+            
+            tf.keras.layers.Conv2D(256, (3,3), activation=tf.keras.layers.LeakyReLU(alpha=0.1),padding = 'same'),
+            tf.keras.layers.BatchNormalization(),
+            tf.keras.layers.Conv2D(256, (3,3), activation=tf.keras.layers.LeakyReLU(alpha=0.1),padding = 'same'),
+            tf.keras.layers.BatchNormalization(),
             tf.keras.layers.MaxPooling2D(2,2),
-            tf.keras.layers.LeakyReLU(alpha=0.1),
-            tf.keras.layers.SpatialDropout2D(0.2),
+
             tf.keras.layers.GlobalAveragePooling2D(),
-            tf.keras.layers.Dense(256, activation=tf.nn.swish),
+
+            tf.keras.layers.Dense(512, activation=tf.nn.swish),
             tf.keras.layers.Dropout(0.5),
             tf.keras.layers.Dense(256, activation=tf.nn.swish),
+            tf.keras.layers.Dropout(0.5),
+            tf.keras.layers.Dense(128, activation=tf.nn.swish),
             tf.keras.layers.Dropout(0.3),
-            tf.keras.layers.Dense(512, activation=tf.nn.swish),
+            tf.keras.layers.Dense(10, activation='softmax')  
           
-            #tf.keras.layers.Dense(500, activation='relu'),
-            tf.keras.layers.Dense(10, activation='softmax')
             ])
         
+        
         #compilacion con funciones de perdida, optimizador y metrica de accuracy
-        self.model.compile(loss='categorical_crossentropy', optimizer=tf.keras.optimizers.Adam(learning_rate=0.0005), metrics=['accuracy'])
+       
+        self.model.compile(loss='categorical_crossentropy', optimizer=tf.keras.optimizers.Adam(0.0003), metrics=['accuracy'])
         
         #modificaciones a las imagenes
         #modifiers = tf.keras.preprocessing.image.ImageDataGenerator(rescale = 1/255)
         
         history = self.model.fit(
             self.dataset,
-            epochs = 40,      
-            callbacks=[tf.keras.callbacks.ReduceLROnPlateau(monitor='loss', factor=0.1, patience=2, verbose=1)]
+            epochs = 30, 
+            #steps_per_epoch=5000,     
+            callbacks=[tf.keras.callbacks.ReduceLROnPlateau(monitor='loss', factor=0.1, patience=5, verbose=1)]
         )
 
         #guardado de la red
         base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-        save_path = os.path.join(base_dir, "SavedFiles", "mi_modelo.h5")
+        save_path = os.path.join(base_dir, "SavedFiles", "model.h5")
         self.model.save(save_path)
         print("Network saved")
         return True
     
-    def classifyTestDeeplake(self):
-        # Get a batch of test images and labels
-        for x_batch, y_batch in self.testset.take(10):  # Taking only 5 samples for testing
-         
-            # Normalize image
-            x_batch = x_batch
-
-            # Make predictions
-            print(x_batch)
-            print(y_batch)
-            predictions = self.model.predict(x_batch)
-            predicted_class = np.argmax(predictions, axis=1)
-
-            print(f"Predicted Animal: {predicted_class[0]}")
-            print("-" * 50)
+    
 
     def uploadImage(self):
         flag = True
+        train_datagen = tf.keras.preprocessing.image.ImageDataGenerator(
+            rescale=1./255
+        )
 
         while flag:
             root = tk.Tk()
@@ -143,19 +161,32 @@ class AnimalNetwork:
             )
 
             if img_path:
-                img = tf.keras.preprocessing.image.load_img(img_path, target_size=(64, 64, 3))  # Prepocesamiento de Imagen - Carga la imagen y la redimensiona
-                img_array = tf.keras.preprocessing.image.img_to_array(img)  # Convierte la imagen a un array       
-                img_array = np.expand_dims(img_array, axis=0)  # expande la dimensión del batch (de 3D a 4D)
+                print("path")
+                print(img_path)
+                img = tf.keras.preprocessing.image.load_img(img_path, target_size=(64,64,3)) 
+                print("img")
+                print(img)
+                img_array = tf.keras.preprocessing.image.img_to_array(img)
+                #img_array = tf.image.convert_image_dtype(img_array, tf.float32)  
+                img_array = tf.cast(img_array, tf.uint8)
+    
+    # Normalize the image to [0, 1]
+                img_array = np.expand_dims(img_array, axis=0)  
+                                
+
                 img_array = img_array / 255.0 # Normaliza la imagen (de 0 a 1) para hacer la predicción
-        
+                print("img_array")   
+                print(img_array)
                 predictions = self.model.predict(img_array)  # hace la predicción
+                print(predictions)
                 predicted_class = np.argmax(predictions, axis=1) # obtiene el índice de la clase de la predicción
                 print(f"Prediction: {predicted_class}")
 
                 # definimos el nombre de los animales 
-                class_labels = ["Cat", "Lynx", "Wolf", "Coyote", "Cheetah", "Jaguar", 
-                                "Chimpanzee", "Orangutan", "Hamster", "Guinea pig"]
-        
+                #class_labels = ["Cat", "Lynx", "Wolf", "Coyote", "Cheetah", "Jaguar", 
+                #                "Chimpanzee", "Orangutan", "Hamster", "Guinea pig"]
+                class_labels = ["Perro", "Caballo", "Elefante", "Mariposa", "Gallina", "Gato", 
+                                "Vaca", "Oveja", "Araña", "Ardilla"]
                 print(f"Predicted Animal: {class_labels[predicted_class[0]]}")
                 print("-" * 50)
             else:
